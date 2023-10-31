284393f0f0725b259c53257f44944665d5dbb17d
Thanos Trai <thatrai@hotmail.com>
2023-10-31 19:32:45 +0200

    connection to mongodb

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -4,17 +4,17 @@ import { HydratedDocument } from 'mongoose';
 
 @Schema()
 export class User {
-    @Prop({type: Number, required:true, unique: true})
+    @Prop({type: Number, required: true, unique: true})
     id: number;
-    @Prop({type: String, required:true, unique:true})
+    @Prop({type: String, required: true, unique:true})
     givenName: string;
-    @Prop({type: String, required:true})
+    @Prop({type: String, required: true})
     surName: string;
-    @Prop({type: Number, required:true})
+    @Prop({type: Number, required: true})
     age: number;
-    @Prop({type: String, required:true, unique: true})
+    @Prop({type: String, required: true, unique: true})
     email: string;
-    @Prop({type: String, required:true})
+    @Prop({type: String, required: true})
     address: string;
     @Prop({type: String, default: ''})
     photoUrl: string;
